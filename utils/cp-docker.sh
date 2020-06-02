6384a1da3c93f1d18784307df05c3181a46eacdd
ccorley <ccorley@us.ibm.com>
2020-06-02 16:38:21 -0500

    create the HL7 and FHIR Kafka topics

---
+++ utils/cp-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker cp create-topics.sh idaas-connect_kafka_1:/opt/kafka_2.12-2.3.0/bin
+docker cp hl7-msgs.txt idaas-connect_kafka_1:/opt/kafka_2.12-2.3.0/bin
+docker cp fhir-msgs.txt idaas-connect_kafka_1:/opt/kafka_2.12-2.3.0/bin
---
+++ utils/create-topics.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+while read p; do
+  topic=HL7_$p
+  echo $topic
+  ./kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic $topic
+done < ./hl7-msgs.txt
+
+while read p; do
+  topic=FHIR_R4_$(echo $p | tr '[:lower:]' '[:upper:]')
+  echo $topic
+  ./kafka-topics.sh --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic $topic
+done < ./fhir-msgs.txt
