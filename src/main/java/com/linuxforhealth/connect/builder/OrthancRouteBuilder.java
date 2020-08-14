8f144a6d5b8ae6d50da61f7ad6bf18b495e5ebd0
ccorley <ccorley@us.ibm.com>
2020-08-14 07:52:36 -0500

    Update OrthancRouteBuilder.java

--- src/main/java/com/linuxforhealth/connect/builder/OrthancRouteBuilder.java
+++ src/main/java/com/linuxforhealth/connect/builder/OrthancRouteBuilder.java
@@ -9,6 +9,7 @@ import com.linuxforhealth.connect.processor.MetaDataProcessor;
 import com.linuxforhealth.connect.support.CamelContextSupport;
 
 import java.net.URI;
+import org.apache.camel.Exchange;
 import org.json.JSONObject;
 
 import org.slf4j.Logger;
@@ -21,8 +22,7 @@ public class OrthancRouteBuilder extends BaseRouteBuilder {
 
     private final Logger logger = LoggerFactory.getLogger(OrthancRouteBuilder.class);
 
-    public final static String POST_ROUTE_ID = "orthanc-post";
-    public final static String GET_ROUTE_ID = "orthanc-get";
+    public final static String ROUTE_ID = "orthanc-post";
 
     @Override
     protected String getRoutePropertyNamespace() {
@@ -36,18 +36,34 @@ public class OrthancRouteBuilder extends BaseRouteBuilder {
         URI orthancRestUri = URI.create(contextSupport.getProperty("lfh.connect.orthanc.rest.uri"));
 
         from("{{lfh.connect.orthanc.uri}}")
-            .routeId(POST_ROUTE_ID)
+            .routeId(ROUTE_ID)
             .marshal().mimeMultipart()
             .removeHeaders("Camel*")
             .removeHeaders("Host")
             .to(orthancServerUri)
             .process(exchange -> {
+                // Get the resulting image id & create uri for downstream image retrieval
                 String body = exchange.getIn().getBody(String.class);
                 JSONObject obj = new JSONObject(body);
                 String id = obj.getString("ID");
-                String imageUri = orthancServerUri+"/"+id+"/preview";
-                exchange.getIn().setBody(imageUri);
-                logger.info("image Uri: "+imageUri);
+                exchange.setProperty("dataUri", orthancServerUri+"/"+id+"/preview");
+
+                // Set up for next call to get image tags
+                exchange.setProperty("location", orthancServerUri+"/"+id+"/simplified-tags");
+                exchange.getIn().removeHeaders("Camel*");
+                exchange.getIn().removeHeaders("Content*");
+                exchange.getIn().setHeader(Exchange.HTTP_METHOD, "GET");
+            })
+            .toD("${exchangeProperty[location]}")
+            .process(exchange -> {
+                JSONObject tags = new JSONObject(exchange.getIn().getBody(String.class));
+                JSONObject result = new JSONObject();
+                result.put("patientId", tags.getString("PatientID"));
+                result.put("patientName", tags.getString("PatientName"));
+                result.put("sourceType", "orthanc");
+                result.put("sourceUri", simple("${exchangeProperty[dataUri]}").evaluate(exchange, String.class));
+                exchange.getIn().setBody(result.toString());
+                logger.info("result: "+result.toString());
             })
             .process(new MetaDataProcessor(routePropertyNamespace))
             .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI);
