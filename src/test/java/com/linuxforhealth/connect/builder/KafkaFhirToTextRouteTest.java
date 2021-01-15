9cd21438a179a6926619ea394044e84f58c20b5a
Scott Carrier <scarrier@us.ibm.com>
2021-01-14 22:14:01 -0500

    Add DiagnosticReport attachment processing

--- src/test/java/com/linuxforhealth/connect/builder/KafkaFhirToTextRouteTest.java
+++ src/test/java/com/linuxforhealth/connect/builder/KafkaFhirToTextRouteTest.java
@@ -29,8 +29,12 @@ public class KafkaFhirToTextRouteTest extends RouteTestSupport {
         mockResult = MockEndpoint.resolve(context, "mock:result");
     }
 
+    /**
+     * Test DocumentReference attachment (2) text extraction
+     * @throws Exception
+     */
     @Test
-    void testRoute() throws Exception {
+    void testRouteDocumentReference() throws Exception {
 
         String msg = context
                 .getTypeConverter()
@@ -42,17 +46,51 @@ public class KafkaFhirToTextRouteTest extends RouteTestSupport {
         String lfhMsgFhirResourceEnc = JsonPath.parse(msg).read("$.data", String.class);
         String lfhMsgFhirResourceDec = new String(Base64.getDecoder().decode(lfhMsgFhirResourceEnc));
 
-        String fhirResAttachEnc = JsonPath.parse(lfhMsgFhirResourceDec).read("$.content[0].attachment.data", String.class);
-        String fhirResAttachDec = new String(Base64.getDecoder().decode(fhirResAttachEnc));
+        String fhirResAttach0Enc = JsonPath.parse(lfhMsgFhirResourceDec).read("$.content[0].attachment.data", String.class);
+        String fhirResAttach0Dec = new String(Base64.getDecoder().decode(fhirResAttach0Enc));
 
-        mockResult.expectedMessageCount(1);
-        mockResult.expectedBodiesReceived(fhirResAttachDec);
+        String fhirResAttach1Enc = JsonPath.parse(lfhMsgFhirResourceDec).read("$.content[1].attachment.data", String.class);
+        String fhirResAttach1Dec = new String(Base64.getDecoder().decode(fhirResAttach1Enc));
+
+        mockResult.expectedMessageCount(2);
+        mockResult.expectedBodiesReceived(fhirResAttach0Dec, fhirResAttach1Dec);
 
         fluentTemplate.to(KafkaFhirToTextRouteBuilder.ROUTE_URI_FHIR_LFH_MSG)
                 .withBody(msg)
                 .send();
 
         mockResult.assertIsSatisfied();
+    }
+
+    /**
+     * Test DiagnosticReport attachment (1) text extraction
+     * @throws Exception
+     */
+    @Test
+    void testRouteDiagnosticReport() throws Exception {
+
+        String msg = context
+                .getTypeConverter()
+                .convertTo(String.class,
+                        TestUtils.getMessage(
+                                "lfh",
+                                "lfh-msg-fhir-r4-diagrpt-inline-attach.json"));
+
+        String lfhMsgFhirResourceEnc = JsonPath.parse(msg).read("$.data", String.class);
+        String lfhMsgFhirResourceDec = new String(Base64.getDecoder().decode(lfhMsgFhirResourceEnc));
+
+        String fhirResAttach0Enc = JsonPath.parse(lfhMsgFhirResourceDec).read("$.presentedForm[0].data", String.class);
+        String fhirResAttach0Dec = new String(Base64.getDecoder().decode(fhirResAttach0Enc));
+
+        mockResult.expectedMessageCount(1);
+        mockResult.expectedBodiesReceived(fhirResAttach0Dec);
+
+        fluentTemplate.to(KafkaFhirToTextRouteBuilder.ROUTE_URI_FHIR_LFH_MSG)
+                .withBody(msg)
+                .send();
+
+        mockResult.assertIsSatisfied();
+
     }
 
 }
