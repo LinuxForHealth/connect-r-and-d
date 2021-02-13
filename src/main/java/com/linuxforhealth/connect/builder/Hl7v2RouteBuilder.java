0d8cdc0aa30070b264fc955d8032f964c60a8896
Pushpa Bhallamudi <55480553+pbhallam@users.noreply.github.com>
2021-02-13 22:36:50 +0800

    Hl7 fhir route (#437)

--- src/main/java/com/linuxforhealth/connect/builder/Hl7v2RouteBuilder.java
+++ src/main/java/com/linuxforhealth/connect/builder/Hl7v2RouteBuilder.java
@@ -5,13 +5,13 @@
  */
 package com.linuxforhealth.connect.builder;
 
-import com.linuxforhealth.connect.processor.Hl7NaaccrProcessor;
-import com.linuxforhealth.connect.processor.MetaDataProcessor;
-
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.linuxforhealth.connect.processor.Hl7NaaccrProcessor;
+import com.linuxforhealth.connect.processor.MetaDataProcessor;
+import com.linuxforhealth.connect.support.FHIRConverter;
 
 
 /**
@@ -19,14 +19,16 @@ import org.slf4j.LoggerFactory;
  */
 public class Hl7v2RouteBuilder extends BaseRouteBuilder {
 
-    public final static String ROUTE_ID = "hl7-v2";
-    public final static String ROUTE_PRODUCER_ID="hl7-v2";
-    public final static String ROUTE_PROPERTY_NAMESPACE = "lfh.connect.hl7-v2";
-    public final static String HTTP_ROUTE_ID = "hl7-v2-http";
-    public final static String NAACCR_ROUTE_ID = "hl7-v2-naaccr";
-    public final static String NAACCR_PRODUCER_ID = "hl7-v2-naaccr-producer";
+  public static final String ROUTE_ID = "hl7-v2";
 
-    private final Logger logger = LoggerFactory.getLogger(Hl7NaaccrProcessor.class);
+  public static final String ROUTE_PRODUCER_ID = "hl7-v2";
+  public static final String ROUTE_PROPERTY_NAMESPACE = "lfh.connect.hl7-v2";
+  public static final String HTTP_ROUTE_ID = "hl7-v2-http";
+  public static final String NAACCR_ROUTE_ID = "hl7-v2-naaccr";
+  public static final String NAACCR_PRODUCER_ID = "hl7-v2-naaccr-producer";
+  public static final String HL7_FHIR_ROUTE_ID = "hl7-v2-fhir";
+  public static final String HL7_FHIR_PRODUCER_ID = "hl7-v2-fhir-producer";
+  private static final Logger LOGGER = LoggerFactory.getLogger(Hl7NaaccrProcessor.class);
 
     @Override
     protected String getRoutePropertyNamespace() {
@@ -42,7 +44,8 @@ public class Hl7v2RouteBuilder extends BaseRouteBuilder {
                 .unmarshal().hl7()
                 .process(new MetaDataProcessor(routePropertyNamespace))
                 .multicast()
-                .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI, "direct:"+NAACCR_ROUTE_ID)
+        .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI, "direct:" + NAACCR_ROUTE_ID,
+            "direct:" + HL7_FHIR_ROUTE_ID)
                 .id(ROUTE_PRODUCER_ID);
 
         //Route for HTTP origin
@@ -51,7 +54,8 @@ public class Hl7v2RouteBuilder extends BaseRouteBuilder {
                 .unmarshal().hl7()
                 .process(new MetaDataProcessor(routePropertyNamespace))
                 .multicast()
-                .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI, "direct:"+NAACCR_ROUTE_ID)
+        .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI, "direct:" + NAACCR_ROUTE_ID,
+            "direct:" + HL7_FHIR_ROUTE_ID)
                 .id(HTTP_ROUTE_ID);
 
         //Route for NAACCR HL7 Subprotocol for Electronic Pathology Reports
@@ -63,11 +67,24 @@ public class Hl7v2RouteBuilder extends BaseRouteBuilder {
                     .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI)
                     .id(NAACCR_PRODUCER_ID)  
                 .otherwise()
-                .log(LoggingLevel.INFO, logger, "non-NAACCR report")
+        .log(LoggingLevel.INFO, LOGGER, "non-NAACCR report")
                     .stop()
                 .end();
-       
-    
+
+    // Route for HL7 to FHIR conversion
+    from("direct:" + HL7_FHIR_ROUTE_ID)
+        .routeId(HL7_FHIR_ROUTE_ID)
+        .choice()
+        .when(simple("${properties:lfh.connect.hl7-v2.convertToFhir:false} == 'true'"))
+        .bean(FHIRConverter.class, "convert")
+        .multicast()
+        .to(FhirR4RouteBuilder.EXTERNAL_FHIR_ROUTE_URI, LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI).id(HL7_FHIR_PRODUCER_ID)
+        .endChoice()
+        .otherwise()
+        .log(LoggingLevel.INFO, LOGGER, "No FHIR conversion required")
+        .end();
+
+
     }
 
     //used as predicate to detect whether to process as an NAACCR message
