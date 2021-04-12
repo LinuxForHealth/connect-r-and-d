1f57be2f0c544c6b39b6b677a1037e6e1cc49f29
Subha Ram <subha.ram@consensyshealth.com>
2021-04-12 13:05:09 -0500

    Added authentication

--- src/main/java/com/linuxforhealth/connect/builder/KaleidoRouteBuilder.java
+++ src/main/java/com/linuxforhealth/connect/builder/KaleidoRouteBuilder.java
@@ -36,11 +36,29 @@ public class KaleidoRouteBuilder extends BaseRouteBuilder {
         from(kaleidoUri)
             .setHeader(Exchange.HTTP_METHOD,simple("GET"))
             .routeId(KALEIDO_ROUTE_ID)
+            .setHeader("Accept", constant("application/json"))
+            .setHeader(Exchange.CONTENT_TYPE, constant(ContentType.APPLICATION_JSON))
+            .setHeader("x-api-key",simple("07191d211e8f66e33db9c7717bf38853973722dba9517fb4d1"))
             .log("Routing to REST")
-            .to("https://jsonplaceholder.typicode.com/todos/1?bridgeEndpoint=true&throwExceptionOnFailure=false")
+            //.to("https://jsonplaceholder.typicode.com/todos/1?bridgeEndpoint=true&throwExceptionOnFailure=false")
+            .to("https://hun-demo.hun-dev.kaleido.cloud/api/v1/ping?bridgeEndpoint=true&throwExceptionOnFailure=false")
             .process(new MetaDataProcessor(routePropertyNamespace))
             .id(KALEIDO_ROUTE_PRODUCER_ID)
-            .log(LoggingLevel.DEBUG, logger, "Response code: ${header.CamelHttpResponseCode}");
+            .log(LoggingLevel.DEBUG, logger, "Response code: ${header.CamelHttpResponseCode}")
+            .choice()
+
+                // Only process successful nlp service responses
+                .when(header("CamelHttpResponseCode").isEqualTo("200"))
+                    .process(new MetaDataProcessor(getRoutePropertyNamespace()))
+                    .to(LinuxForHealthRouteBuilder.STORE_AND_NOTIFY_CONSUMER_URI)
+                .endChoice()
+
+                .otherwise()
+                    .log(LoggingLevel.ERROR, logger, "NLP Service error response code: ${header.CamelHttpResponseCode}")
+                    .stop()
+                .endChoice()
+
+            .end();
     }
 
 }
